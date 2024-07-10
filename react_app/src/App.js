79e22f80e6c659746b52ee96e4844aa96babb542
MatheusBorgez <matheusab594@gmail.com>
2024-07-10 02:10:18 -0300

    remove lixo gerado

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
+
+  const [ counter, setCounter ] = useState(0);
+
+  function soma() {
+    setCounter(counter + 1);
+  }
+
+  function subtracao() {
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <h1>{counter}</h1>
+    <button onClick={soma}> + </button>
+    <button onClick={subtracao}> - </button>
+    </>
+
   );
 }
 
